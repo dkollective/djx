c51231b6bdd864d18681546696f3edbebdb16c51
LBrinkmann <com@levin-brinkmann.de>
2019-03-07 20:30:00 +0100

    Seperate store and utils.

---
+++ djx/store.py
@@ -0,0 +1,54 @@
+import md5
+import os
+from urllib.request import urlretrieve
+from shutil import copyfile
+
+
+DATA_TEMP = os.environ['DJX_DATA_TEMP']
+DATA_STORE = os.environ['DJX_DATA_STORE']
+
+
+def create_temp_path(remote):
+    file_id = md5.hex(remote).hexdigest()
+    return os.path.join(DATA_TEMP, file_id)
+
+
+def load_file(remote, temp):
+    if remote[0:5] == 'http':
+        urlretrieve(remote, temp)
+    elif remote[0] == '/':
+        copyfile(remote, temp)
+    else:
+        ValueError(f'Unkown data source {remote}')
+
+
+def store_file(temp, remote):
+    if remote[0] == '/':
+        copyfile(temp, remote)
+    else:
+        ValueError(f'Unkown data store {remote}')
+
+
+def check_temp(temp_path):
+    return os.path.isfile(temp_path)
+
+
+def get_data(remote):
+    temp_path = create_temp_path(remote)
+    if not check_temp(temp_path):
+        load_file(remote, temp_path)
+    return temp_path
+
+
+def get_all_data(data):
+    return [get_data(remote) for remote in data]
+
+
+def store_data(data):
+    remote_paths = {}
+    for name, temp_path in data.items():
+        _, extension = os.path.splitext(temp_path)
+        remote_path = os.path.join(DATA_STORE, name + "." + extension)
+        store_file(temp_path, remote_path)
+        remote_paths[name] = remote_path
+    return remote_paths
--- djx/utils.py
+++ djx/utils.py
@@ -2,13 +2,7 @@ import importlib
 import yaml
 import subprocess
 import sys
-import md5
 import os
-from urllib.request import urlretrieve
-from shutil import copyfile
-
-
-DATA_FOLDER = os.environ['DJX_DATA']
 
 
 def get_method(name):
@@ -18,52 +12,6 @@ def get_method(name):
     module = importlib.import_module(module_name)
     func = getattr(module, method_name)
     return func
-
-
-def get_local_path(remote):
-    file_id = md5.hex(remote).hexdigest()
-    return os.path.join(DATA_FOLDER, file_id)
-
-
-def load_file(remote, local):
-    if remote[0:5] == 'http':
-        urlretrieve(remote, local)
-    elif remote[0] == '/':
-        copyfile(remote, local)
-    else:
-        ValueError(f'Unkown data source {remote}')
-
-
-def store_file(local, remote):
-    if remote[0] == '/':
-        copyfile(local, remote)
-    else:
-        ValueError(f'Unkown data store {remote}')
-
-
-def check_local(local_path):
-    return os.path.isfile(local_path)
-
-
-def get_data(remote):
-    local_path = get_local_path(remote)
-    if not check_local(local_path):
-        load_file(remote, local_path)
-    return local_path
-
-
-def get_all_data(data):
-    return [get_data(remote) for remote in data]
-
-
-def store_data(data, ouput_prefix):
-    remote_paths = {}
-    for name, local_path in data.items():
-        _, extension = os.path.splitext(local_path)
-        remote_path = os.path.join(ouput_prefix, name + "." + extension)
-        store_file(local_path, remote_path)
-        remote_paths[name] = remote_path
-    return remote_paths
 
 
 def get_file(func):
@@ -77,7 +25,7 @@ def load_yaml(file_name):
         return yaml.load(f.read())
 
 
-def get_git_info(entry, **kwargs):
+def get_git_info(entry):
     func = get_method(entry)
     folder = os.path.dirname(get_file(func))
     commit = subprocess.check_output(

91186a7cd7eac43b43f5252080d6e041786d3de2
LBrinkmann <com@levin-brinkmann.de>
2019-03-12 19:03:13 +0100

    Add epochs to example.

--- djx/example/src/iris.py
+++ djx/example/src/iris.py
@@ -3,7 +3,7 @@ import pandas as pd
 import uuid
 from time import sleep
 from djx import record
-from djx import model
+from djx import mstore
 from structlog import get_logger
 from sklearn.model_selection import StratifiedKFold
 from sklearn.linear_model import SGDClassifier
@@ -19,33 +19,41 @@ def load_dataset(dataset, test_size, target_column, feature_columns):
     return X, y
 
 
-def cross_val_model(X, y, model_args, cross_val_args):
+def cross_val(X, y, training_args, model_args, cross_val_args):
     cv = StratifiedKFold(**cross_val_args)
     classes = y.unique()
+
+    epochs = training_args['epochs']
+    batch_num = training_args['batch_num']
 
     for i, (train_index, test_index) in enumerate(cv.split(X, y)):
         record.bind(cross_val_split=i)
         clf = SGDClassifier(**model_args)
         X_train, X_test = X.iloc[train_index], X.iloc[test_index]
         y_train, y_test = y.iloc[train_index], y.iloc[test_index]
-        for j in range(10):
-            record.bind(batch=j)
-            clf.partial_fit(
-                X_train.iloc[j::10], y_train.iloc[j::10], classes=classes)
-            sleep(1)
-            score = clf.score(X_test, y_test)
-            record.rec('partial fit', accuracy=score)
+        tdata = 0
+        for e in range(training_args['epochs']):
+            for j in range(10):
+                X_batch = X_train.iloc[j::batch_num]
+                y_batch = y_train.iloc[j::batch_num]
+                batch_size = len(y_batch)
+                clf.partial_fit(X_batch, y_batch, classes=classes)
+                sleep(0.2)
+                score = clf.score(X_test, y_test)
+                tdata += batch_size
+                record.bind(batch=j, epoch=j, datasize=tdata)
+                record.rec('partial fit', accuracy=score)
         record.rec('complete fit', accuracy=score)
 
 
-def fit_and_save(X, y, model_args):
+def fit_save(X, y, model_args):
     local_model_path = os.path.join(DATA_FOLDER, uuid.uuid4().hex + '.pkl')
     clf = SGDClassifier(**model_args)
     clf.fit(X, y)
-    model.save_mode(clf, local_model_path)
+    mstore.save(clf, local_model_path)
 
 
 def cv_fit_save(data, *, model_args, dataset_args, cross_val_args):
     X, y = load_dataset(data['dataset'], **dataset_args)
-    cross_val_model(X, y, model_args, cross_val_args)
-    fit_and_save(X, y, model_args)
+    cross_val(X, y, model_args, cross_val_args)
+    fit_save(X, y, model_args)
